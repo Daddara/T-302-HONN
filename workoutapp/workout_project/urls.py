b7673ac62a818092afff49de63228576a9626205
Katrín Viktoría Hjartardóttir <katrinviktoria@Katrins-MacBook-Pro.local>
2020-09-06 15:50:23 +0000

    moving the html files

--- workoutapp/workout_project/urls.py
+++ workoutapp/workout_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('workout.css/', include('workout.css.urls')),
+    path('main.css/', include('main.css.urls')),
     path('admin/', admin.site.urls),
 ]
