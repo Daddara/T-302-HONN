250832e0d459f2bbd8ddfec0218ca5af99974cde
madsaken <haffisaken@gmail.com>
2020-09-11 12:02:04 +0000

    Changed according to comments on previous merge request.

--- workoutapp/user/models.py
+++ workoutapp/user/models.py
@@ -1,18 +1,21 @@
 from django.contrib.auth.models import User
 from django.db import models
+import datetime
 
 
 class Messages(models.Model):
     Sender = models.ForeignKey(User,  on_delete=models.CASCADE, related_name='Sender')
     Receiver = models.ForeignKey(User,  on_delete=models.CASCADE, related_name='Receiver')
-    Messages = models.CharField(max_length=250, blank=True)
-    SenderDate = models.DateField(blank=True)
-    Seen = models.BooleanField(blank=True)
+    Messages = models.CharField(max_length=250, default="")
+    SendDate = models.DateTimeField(auto_now=True)
+    Seen = models.BooleanField(default="")
 
 class UserInfo(models.Model):
     User = models.ForeignKey(User,  on_delete=models.CASCADE)
-    FirstName = models.CharField(max_length=250, blank=True)
-    LastName = models.CharField(max_length=250, blank=True)
-    BirthYear = models.DateField(blank=True)
-    Image = models.CharField(max_length=250, blank=True)
-    CreatedAt = models.DateField(blank=True)
+    FirstName = models.CharField(max_length=20, default="")
+    LastName = models.CharField(max_length=20, default="")
+    BirthYear = models.DateTimeField(default=datetime.date.today())
+    Image = models.CharField(
+        max_length=250,
+        default="https://www.vhv.rs/dpng/d/256-2569650_men-profile-icon-png-image-free-download-searchpng.png")
+    CreatedAt = models.DateTimeField(auto_now=True)
--- workoutapp/workout/models.py
+++ workoutapp/workout/models.py
@@ -1,42 +1,54 @@
 from django.contrib.auth.models import User
 from django.db import models
+import datetime
 
 # Create your models here.
 class Category(models.Model):
-    Name = models.CharField(max_length=250, blank=False)
+    Name = models.CharField(max_length=20, default="", unique=True)
 
 class Workout(models.Model):
     User = models.ForeignKey(User,  on_delete=models.CASCADE)
-    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    Name = models.CharField(max_length=250, blank=True)
-    CreatedAt = models.DateField(blank=True)
-    Image = models.CharField(max_length=250, blank=True)
-    Likes = models.IntegerField(blank=True)
-    Dislikes = models.IntegerField(blank=True)
+    Category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    Name = models.CharField(max_length=20, default="")
+    CreatedAt = models.DateTimeField(auto_now=True)
+    Image = models.CharField(
+        max_length=250,
+        default="https://www.vhv.rs/dpng/d/256-2569650_men-profile-icon-png-image-free-download-searchpng.png")
+    Likes = models.IntegerField(default=0)
+    Dislikes = models.IntegerField(default=0)
 
 class Comment(models.Model):
     Workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
     Commenter = models.ForeignKey(User, on_delete=models.CASCADE)
-    Message = models.CharField(max_length=250, blank=True)
+    Message = models.CharField(max_length=250, default="")
 
 class Equipment(models.Model):
-    Name = models.CharField(max_length=250, blank=True)
-    Description = models.CharField(max_length=250, blank=True)
-    Image = models.CharField(max_length=250, blank=True)
+    Name = models.CharField(max_length=20, default="")
+    Description = models.CharField(max_length=250, default="")
+    Image = models.CharField(
+        max_length=250,
+        default="https://www.vhv.rs/dpng/d/256-2569650_men-profile-icon-png-image-free-download-searchpng.png")
 
 class Exercise(models.Model):
-    Title = models.CharField(max_length=250, blank=True)
-    Description = models.CharField(max_length=250, blank=True)
-    Image = models.CharField(max_length=250, blank=True)
-    Equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    Title = models.CharField(max_length=20, default="")
+    Creator = models.ForeignKey(User,  null=True, on_delete=models.SET_NULL)
+    Description = models.CharField(max_length=250, default="")
+    Image = models.CharField(
+        max_length=250,
+        default="https://www.vhv.rs/dpng/d/256-2569650_men-profile-icon-png-image-free-download-searchpng.png")
+    Equipment = models.ForeignKey(Equipment, null=True, on_delete=models.SET_NULL)
+    Public = models.BooleanField(default=False)
 
 class UnitType(models.Model):
-    Name = models.CharField(max_length=250, blank=True)
-    Unit = models.CharField(max_length=250, blank=True)
+    Name = models.CharField(max_length=20, default="")
+    Unit = models.CharField(max_length=20, default="")
 
 class WorkoutManager(models.Model):
     Workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
-    Exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
-    Unit = models.ForeignKey(UnitType, on_delete=models.CASCADE)
-    Reps = models.IntegerField(blank=True)
-    Quantity = models.IntegerField(blank=True)
+    Exercise = models.ForeignKey(Exercise, null=True, on_delete=models.SET_NULL)
+    Unit = models.ForeignKey(UnitType, null=True, on_delete=models.SET_NULL)
+    Reps = models.IntegerField(default=0)
+    Quantity = models.IntegerField(default=0)
+
+class Meta:
+    unique_together = ('creator', 'Title',)
