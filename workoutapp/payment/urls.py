731e53d4cd34d936ac8a4f429cd2ec5c90b7043e
Ægir Hauksson <agir96@gmail.com>
2020-09-19 15:54:23 +0000

    Fixed javascript error handling

--- workoutapp/payment/urls.py
+++ workoutapp/payment/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('shop-fitcoins/', views.purchase_fitcoins, name='purchase-fitcoins'),
-    path('overview/<int:product_id>/', views.product_overview, name="overview"),
-    path('payment-confirmation/', views.payment_complete, name="validate-payment"),
-    path('order-complete/<int:order_id>/', views.completion_page, name="payment-success"),
-    path('product-404/<int:product_id>/', views.product_not_found, name="product-404"),
-    path('invalid-payment/<int:invalid_id>/', views.payment_invalid, name="invalid-payment"),
+    path('product-overview/<int:product_id>/', views.product_overview, name="overview"),
+    path('validate-payment/', views.payment_validation, name="validate-payment"),
+    path('payment-success/<int:order_id>/', views.payment_success, name="payment-success"),
+    path('test-product-404/<int:product_id>/', views.service_product_not_found, name="product-404"),
+    path('test-invalid-payment/<int:invalid_id>/', views.service_payment_invalid, name="invalid-payment"),
     path('test-payment-confirmation/<int:product_id>/<int:error_insertion>/', views.service_payment_complete, name='test-payment')
 ]
--- workoutapp/payment/views.py
+++ workoutapp/payment/views.py
@@ -25,33 +25,39 @@ def product_overview(request, product_id):
 
 
 @login_required
-def payment_complete(request):
+def payment_validation(request):
     """This view is called via javascript method completeOrder in order_overview.html file after the payment has been
     processed in the paypal client. The purpose of this view is to check if all the payment data received from paypal is
     valid, and returns an appropriate response"""
+    # If someone is trying to access the url without a json body
     try:
         body = json.loads(request.body)
     except JSONDecodeError:
-        return JsonResponse({'Error': 1}, status=400)
+        return render(request, '400.html', context={'error': 'Something unexplainable went wrong.'}, status=400)
     currency = str(body['currency'])
     amount = round(float(body['amount']), 2)
 
     # If someone tampered with currency
     if not currency == 'USD':
         print("currency invalid")
-        return JsonResponse({'Error': 2}, status=400)
+        return render(request, '400.html', context={'error': 'You have paid with the incorrect currency. Please'
+                                                             'contact the administrators of the website via the '
+                                                             'support page.'}, status=400)
     product_id = int(body['productID'])
 
     # Get product by id, return 404 if doesn't exist
     try:
         product = Product.objects.get(pk=product_id)
     except Product.DoesNotExist:
-        return JsonResponse({'productID': product_id}, status=404)
+        return render(request, '404.html', context={'error': 'No product with id ' + product_id + " exists."},
+                      status=404)
 
     # If someone tampered with the price of the product
     if not float(amount) == float(product.price_usd()):
         print("amount invalid")
-        return JsonResponse({'Error': 1}, status=400)
+        return render(request, '400.html', context={'error': 'The price of the product was tampered with. The order'
+                                                             'was canceled. Please contact support, or refund via '
+                                                             'paypal.'}, status=400)
 
     # Everything checks out -> Create an order
     new_order = Order.objects.create(product=product, customer=request.user, price=product.price_usd())
@@ -60,11 +66,11 @@ def payment_complete(request):
     user_wallet = Wallet.objects.get(user=user)  # Fetching user wallet
     user_wallet.add_balance(product.fitcoins)  # Adding to user balance
 
-    return JsonResponse({'orderID': new_order.id}, status=201, safe=False)
+    return JsonResponse({'orderID': new_order.id}, status=201)
 
 
 @login_required
-def completion_page(request, order_id):
+def payment_success(request, order_id):
     try:
         order = Order.objects.get(pk=order_id)
     except Order.DoesNotExist:
@@ -74,12 +80,12 @@ def completion_page(request, order_id):
 
 
 @login_required
-def product_not_found(request, product_id):
+def service_product_not_found(request, product_id):
     return render(request, '404.html', context={'error': 'No product with id ' + product_id + " exists."})
 
 
 @login_required
-def payment_invalid(request, invalid_id):
+def service_payment_invalid(request, invalid_id):
     # invalid 1 : Amount tampering
     if invalid_id == 1:
         return render(request, '400.html', context={'error': 'The price of the product was tampered with. The order'
