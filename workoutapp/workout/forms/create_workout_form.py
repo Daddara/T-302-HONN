d37ebc5c3f73548c2475dd82b321688bc2e1f97c
unknown <valgerdur19@ru.is>
2020-09-20 13:57:34 +0000

    valgerdur-asgeirsdottir/05-create-workout

--- workoutapp/workout/forms/create_workout_form.py
+++ workoutapp/workout/forms/create_workout_form.py
@@ -2,19 +2,17 @@ from django import forms
 from ..models import Workout, Exercise, WorkoutManager, UnitType, Category, User
 
 
-
-class CreateWorkoutForm(forms.Form):
+class CreateWorkoutForm(forms.ModelForm):
     workout_name = forms.CharField(max_length=30, required=True)
     category = forms.ModelChoiceField(queryset=Category.objects.all())
     image = forms.ImageField()
 
     class Meta:
         model = Workout
-        fields = ('workout_name', 'category', 'user', 'created_at', 'image', 'likes', 'dislikes')
+        fields = ('workout_name', 'category', 'image')
 
 
-class WorkoutManagerForm(forms.Form):
-    workout = forms.ModelChoiceField(queryset=Workout.objects.all())
+class WorkoutManagerForm(forms.ModelForm):
     exercise = forms.ModelChoiceField(queryset=Exercise.objects.all())
     unit = forms.ModelChoiceField(queryset=UnitType.objects.all())
     reps = forms.IntegerField(required=True)
@@ -22,4 +20,5 @@ class WorkoutManagerForm(forms.Form):
 
     class Meta:
         model = WorkoutManager
-        fields = ('workout', 'exercise', 'unit', 'reps', 'quantity')
+        exclude = ('workout', )
+        fields = ('exercise', 'unit', 'reps', 'quantity')
--- workoutapp/workout/views.py
+++ workoutapp/workout/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render, redirect
 # Create your views here.
 from django.http import HttpResponse
+from .models import Workout, WorkoutManager, Exercise
 from .forms.create_workout_form import CreateWorkoutForm, WorkoutManagerForm
 from .forms.create_exercise_form import ExerciseForm
 
 
 def create_workout(request):
     if request.method == 'POST':
-        workout_form = CreateWorkoutForm(data=request.POST)
-        workout_man_form = WorkoutManagerForm(data=request.POST)
-        exercise_form = ExerciseForm(data=request.GET)
-        if workout_form.is_valid() and workout_man_form.is_valid() and exercise_form.is_valid():
-            workout_form.save()
-            workout_man_form.save()
-            exercise_form.save()
-            workout_man_form.workout = workout_form
-            workout_man_form.exercise = exercise_form
+        workout_form = CreateWorkoutForm(data=request.POST, instance=Workout())
+        workout_man_form = [WorkoutManagerForm(data=request.POST, prefix=str(x),
+                                               instance=WorkoutManager()) for x in range(0, 3)]
+        if workout_form.is_valid() and all[(wm.is_valid() for wm in workout_man_form)]:
+            new_workout = workout_form.save()
+            for wm in workout_man_form:
+                new_wm = wm.save(commit=False)
+                new_wm.workout = new_workout
+                new_wm.save()
             return redirect('user/placeholder_page.html')
 
         return render(request, 'createWorkout/create_workout.html', {
-            'form': CreateWorkoutForm(), 'errors': workout_form.errors})
+            'workout_form': CreateWorkoutForm(), 'workout_man_form': WorkoutManagerForm(), 'errors': workout_form.errors})
 
     else:
-        form = CreateWorkoutForm()
-    return render(request, 'createWorkout/create_workout.html', {'form': form})
+        workout_form = CreateWorkoutForm(instance=Workout)
+        workout_man_form = [WorkoutManagerForm(prefix=str(x),
+                                               instance=WorkoutManager()) for x in range(0, 3)]
+    return render(request, 'createWorkout/create_workout.html',
+                  {'workout_form': workout_form , 'workout_man_form': workout_man_form})
+
+
+def edit_workout(request):
 
 
 def create_exercise(request):
