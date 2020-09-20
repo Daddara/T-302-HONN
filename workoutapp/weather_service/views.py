efde21ad460bc2d48779d52921ad110720e6c5b0
xFrednet <xFrednet@gmail.com>
2020-09-20 16:31:54 +0000

    Working GUI

--- workoutapp/weather_service/views.py
+++ workoutapp/weather_service/views.py
@@ -11,15 +11,18 @@ from .gateway.weather_gateway import WeatherApiWeatherGateway
 # Create your views here.
 def forecast(_request):
     gateway = WeatherApiWeatherGateway()
-    # In order to allow non-dict objects to be serialized set the safe parameter to False.
     data, err = gateway.get_weather_forecast('Reykjavik')
 
+    # In order to allow non-dict objects to be serialized set the safe parameter to False.
     return HttpResponse(data.to_j(), content_type='application/json')
 
 
 def current(_request):
     gateway = WeatherApiWeatherGateway()
 
+    # We are currently hard coding Reykjavik. We would need to add a location service
+    # or something else to request the location and that would just make
+    # this class more complicated and the gateway less clear
     data, err = gateway.get_weather_current('Reykjavik')
 
     return JsonResponse(data.__dict__, safe=False)
