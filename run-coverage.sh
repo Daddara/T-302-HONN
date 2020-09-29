75d9ddecba0fbfbee8fb4a51819e7e95845ae493
xFrednet <xFrednet@gmail.com>
2020-09-29 15:45:31 +0000

    Fixing run-coverage.sh to return non zero on fail #79

--- run-coverage.sh
+++ run-coverage.sh
@@ -6,8 +6,19 @@ coverage erase
 
 # Run tests
 coverage run manage.py test
+status1=$?
 coverage run --append -m unittest discover -p *_test.py
+status2=$?
+
+if test $status1 -eq 0 -a $status2 -eq 0
+then
+	status=0
+else
+	status=1
+fi
 
 # Report
 coverage html --directory ../htmlcov
 coverage report
+
+exit $status
