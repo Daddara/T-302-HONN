424e07487366d27e4cc6ba68a5059d902eb5c7d8
madsaken <haffisaken@gmail.com>
2020-10-04 23:33:08 +0000

    Dependency injection finished #77

--- workoutapp/weather_service/__init__.py
+++ workoutapp/weather_service/__init__.py
@@ -1,4 +1,5 @@
 from .gateway.weather_gateway import WeatherApiWeatherGateway
 
-Gateway = WeatherApiWeatherGateway()
+
+GATEWAY = WeatherApiWeatherGateway()
 
--- workoutapp/weather_service/views.py
+++ workoutapp/weather_service/views.py
@@ -2,16 +2,12 @@ import json
 from django.core import serializers
 from django.http import HttpResponse
 from django.http import JsonResponse
-from .__init__ import Gateway
+from .__init__ import GATEWAY
 # Create your views here.
 
 
-def setter(instance):
-    return instance
-
-
 def forecast(_request):
-    data, err = gateway.get_weather_forecast('Reykjavik')
+    data, err = GATEWAY.get_weather_forecast('Reykjavik')
     # In order to allow non-dict objects to be serialized set the safe parameter to False.
     return HttpResponse(data.to_j(), content_type='application/json')
 
@@ -20,8 +16,8 @@ def current(_request):
     # We are currently hard coding Reykjavik. We would need to add a location service
     # or something else to request the location and that would just make
     # this class more complicated and the gateway less clear
-    data, err = gateway.get_weather_current('Reykjavik')
+    data, err = GATEWAY.get_weather_current('Reykjavik')
     return JsonResponse(data.__dict__, safe=False)
 
 
-gateway = setter(Gateway)
+
