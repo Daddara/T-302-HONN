ca1cafd9098341a7eec499bfce390dabacf62705
xFrednet <xFrednet@gmail.com>
2020-10-17 14:25:32 +0000

    Added frontend event saving in logging backend #86

--- logging_server/frontend_logger/views.py
+++ logging_server/frontend_logger/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from frontend_logger.forms.frontend_event_form import FrontendEventForm
+from frontend_logger.models import FrontendEvent, FRONTEND_EVENT_TYPES
 # Create your views here.
+
+def log_frontend_event(request):
+    if request.method != 'POST':
+        return HttpResponse(status=200)
+    
+    form = FrontendEventForm(data=request.POST)
+    if not form.is_valid():
+        return HttpResponse(status=400)
+    if form.cleaned_data['event_type'] not in FRONTEND_EVENT_TYPES:
+        return HttpResponse(status=400)
+
+    session_meta = form.cleaned_data['session_meta']
+    if len(session_meta) == 0:
+        session_meta = None
+
+    event = FrontendEvent(
+        event_type=form.cleaned_data['event_type'],
+        user_id=form.cleaned_data['user_id'],
+        current_url=form.cleaned_data['current_url'],
+        event_meta=form.cleaned_data['event_meta'],
+        session_meta=session_meta
+    )
+    event.save()
+    return HttpResponse(status=200)
+
+
