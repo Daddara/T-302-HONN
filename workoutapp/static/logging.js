7067b12b728a7bfbbc6a321b6b536ca27755d7a1
xFrednet <xFrednet@gmail.com>
2020-10-17 17:11:47 +0000

    Logging the like/dislike actions #86

--- workoutapp/static/logging.js
+++ workoutapp/static/logging.js
@@ -15,6 +15,35 @@ function logButtonPress(buttonObj) {
     sendLog(data)
 }
 
+function logErrorMsg(src, msg) {
+    meta = {
+        'src': src,
+        'message': msg.substr(0, 512)
+    };
+
+    let data = new FormData();
+    data.append('event_type', 'JS_ERROR');
+    data.append('current_url', window.location.href);
+    data.append('event_meta', JSON.stringify(meta));
+
+    sendLog(data)
+}
+
+function logException(src, exception) {
+    meta = {
+        'src': src,
+        'name': exception.name,
+        'message': exception.message.substr(0, 512)
+    };
+
+    let data = new FormData();
+    data.append('event_type', 'JS_ERROR');
+    data.append('current_url', window.location.href);
+    data.append('event_meta', JSON.stringify(meta));
+
+    sendLog(data)
+}
+
 function sendLog(payload) {
     if (!logging_active) {
         return;
@@ -23,15 +52,22 @@ function sendLog(payload) {
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState === 4) {
-            if (xmlHttp.status !== 200) {
-                console.log(xmlHttp)
+            if (xmlHttp.status === 404) {
+                // Just disable the service if it is offline
+                // We're tracking here this means we want to be sneaky ;P
+                logging_active = false;
+            }
+            else if (xmlHttp.status !== 200) {
+                // Do you see a `Logging failed: 0` message in the console?
+                // This means you added a lid to an element that reloads the site. This doesn't work sorry
+                // Just remove it and forget that you where ever here. This script doesn't exist...
+                console.log("Logging failed:", xmlHttp.status)
             }
         }
     };
 
     xmlHttp.open("POST", logging_url, true); // true for asynchronous
     xmlHttp.send(payload);
-    console.log("Bye bye");
 }
 
 $("[lid]").click(function() {
