e5de6360b2cc05c8111509dca18969ba73c96a8c
Ægir Hauksson <aegir96@gmail.com>
2020-11-01 18:54:56 +0000

    Added weather service as main entry point

--- workoutapp/weather_service/views.py
+++ workoutapp/weather_service/views.py
@@ -2,12 +2,12 @@ import json
 from django.core import serializers
 from django.http import HttpResponse
 from django.http import JsonResponse
-from .__init__ import WEATHER_GATEWAY_STUB, WEATHER_API_WEATHER_GATEWAY
+from .__init__ import WEATHER_SERVICE
 # Create your views here.
 
 
 def forecast(_request):
-    data, err = WEATHER_API_WEATHER_GATEWAY.get_weather_forecast('Reykjavik')
+    data, err = WEATHER_SERVICE.get_forecast('Reykjavik')
     # In order to allow non-dict objects to be serialized set the safe parameter to False.
     if data is None:
         return JsonResponse({}, status=400)
@@ -18,7 +18,7 @@ def current(_request):
     # We are currently hard coding Reykjavik. We would need to add a location service
     # or something else to request the location and that would just make
     # this class more complicated and the gateway less clear
-    data, err = WEATHER_API_WEATHER_GATEWAY.get_weather_current('Reykjavik')
+    data, err = WEATHER_SERVICE.get_weather('Reykjavik')
     if data is None:
         return JsonResponse({}, status=400)
     return JsonResponse(data.__dict__, safe=False)
